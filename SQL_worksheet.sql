e9f1ee1c23fd7c9e096cbea6c5d0b5ef4d948ca5
Sine Gökhan <101671498+sinegkhnn@users.noreply.github.com>
2022-12-24 16:43:30 +0300

    SQL (ödev_1)

---
+++ SQL_worksheet.sql
@@ -0,0 +1,11 @@
+SELECT title,description FROM film;
+Select * from film where length>60 and length<75;
+Select * From film where rental_rate = 0.99 and (replacement_cost = 12.99 or replacement_cost = 28.99);
+Select last_name from customer where first_name = 'Mary';
+Select * from film where length <50 and (rental_rate = 2.99 or rental_rate != 4.99);
+
+
+
+
+
+
